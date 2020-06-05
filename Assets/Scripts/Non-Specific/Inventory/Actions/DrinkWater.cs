8315bf970d7e6dc18d004ac97e173c6f58facc86
Peter Drake <drake@lclark.edu>
2020-06-04 17:36:44 -0700

    Imported Canvi, excised MoreMountains where trivial.

---
+++ Assets/Scripts/Non-Specific/Inventory/Actions/DrinkWater.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Drink Water Action", menuName = "Items/Actions/Drink")]
+public class DrinkWater : ItemAction
+{
+    
+    public override bool Use(ref Item i)
+    {
+        if (i.GetType() != typeof(Drinkable)) return false;
+
+        Drinkable d = (Drinkable) i;
+        Logger.Instance.Log("Player drank: "+i.name);
+        if (d.killPlayer) Systems.Status.PlayerDeath(d.DeathMessage);
+        Systems.Status.AffectHydration(d.hydrationChange);
+        
+        return true;
+    }
+}
