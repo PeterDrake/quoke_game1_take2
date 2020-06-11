29516d19a63b9ad39f411280dbde72d650121802
Annabel Paris <aparis@lclark.edu>
2020-06-11 16:56:16 -0700

    worked on throwing away gun

---
+++ Assets/Scripts/Non-Specific/Inventory/Actions/ThrowAwayGun.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Throw Away Gun Action", menuName = "Items/Actions/Gun")]
+public class ThrowAwayGun : ItemAction
+{
+    public override bool Use(ref Item i)
+    {
+        
+        if (Systems.Inventory.HasItem((i = Resources.Load<Item>("items/Gun")),1))
+        {
+            Systems.Inventory.RemoveItem(i,1);
+        }
+        
+        return false;
+    }
+}
