745da15ae7de2459babe3d27c1bc725179087bcd
Blythe <blytheballesteros@gmail.com>
2020-06-16 12:19:14 -1000

    Hangtag action

---
+++ Assets/Scripts/WaterHeaterMiniGame/ViewHangTag.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewHangTag : MonoBehaviour
+{
+    public GameObject Hangtag;
+    public Button Close;
+
+    private bool isOpen;
+
+    private void Start()
+    {
+        //Hangtag.SetActive(false);
+        //isOpen = false;
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("Before isOPen is " + isOpen);
+        if (!isOpen)
+        {
+            Hangtag.SetActive(true);
+            isOpen = true;
+            Debug.Log("Click to open & is open should be true" + isOpen);
+        }
+    }
+
+    public void closeHangTag()
+    {
+        if (isOpen)
+        {
+            Hangtag.SetActive(false);
+            isOpen = false;
+        }
+        Debug.Log("Close & isOpen = " + isOpen);
+    }
+}
