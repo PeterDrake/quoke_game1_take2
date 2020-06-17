00913762080b1414a2249f63ec2028f36496df5d
Lanaparezanin <lanaparezanin@lclark.edu>
2020-06-17 23:56:52 +0200

    Earthquake additions

--- Assets/Scripts/Level 1/EarthquakeColliderCheck.cs
+++ Assets/Scripts/Level 1/EarthquakeColliderCheck.cs
@@ -7,23 +7,26 @@ public class EarthquakeColliderCheck : MonoBehaviour
 {
     public UnityEvent OnEnter;
 
-    public UnityEvent OnExit;
+    //public UnityEvent OnExit;
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Logger.Instance.Log("Player entered area of: "+name);
             OnEnter.Invoke();
+            // if other two are triggered
+                    //call for an earthquake
         }
     }
 
+    /*
     public void OnTriggerExit(Collider other)    //Destroy earthquake script and only leave aftershock???
     {
         if (other.CompareTag("Player"))
         {
+            //if earthquake happened, destroy script
             Logger.Instance.Log("Player exited area of: "+name);
             OnExit.Invoke();
         }
 
-    }
+    }*/
 }
