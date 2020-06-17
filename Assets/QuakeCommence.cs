e451bc2c7ce01842e6512be468f30a5f3cc287b8
Annabel Paris <aparis@lclark.edu>
2020-06-17 16:20:26 -0700

    earthquake starts after exploring house

---
+++ Assets/QuakeCommence.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuakeCommence : MonoBehaviour
+{
+
+    private int Explored;
+
+    public void incrementExplored()
+    {
+        Explored = Explored + 1;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Explored = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Explored >= 2)
+            {
+                QuakeManager.Instance.TriggerQuake();
+            }
+        }
+    }
+}
