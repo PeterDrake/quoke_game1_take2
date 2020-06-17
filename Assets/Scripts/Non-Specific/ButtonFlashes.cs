129d2eff02f48003b120e96c7af63efcd098d882
Lanaparezanin <lanaparezanin@lclark.edu>
2020-06-17 19:41:09 +0200

    Instructions button now flashes

---
+++ Assets/Scripts/Non-Specific/ButtonFlashes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFlashes : MonoBehaviour
+{
+    public Button.ButtonClickedEvent onClick;
+    public Button theButton;
+    
+   
+    private void Start()
+    {
+        StartCoroutine(BlinkText());
+    }
+
+    public IEnumerator BlinkText()
+    {
+        while (true)
+        {
+            theButton.GetComponent<Image>().color = Color.red;
+            yield return new WaitForSeconds(0.3f);
+            theButton.GetComponent<Image>().color = Color.white;
+            //theButton.colors = Color.white;
+            yield return new WaitForSeconds(0.15f);
+        }
+    }
+}
