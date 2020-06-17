bf061c5d4bfd6db825d5feb91d4b69cdd17cd300
Blythe <blytheballesteros@gmail.com>
2020-06-16 15:49:19 -1000

    check marks work and steps in order

---
+++ Assets/Scripts/WaterHeaterMiniGame/EachStepAction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EachStepAction : MonoBehaviour
+{
+    public GameObject check;
+    public GameObject current;
+
+    public WaterHeaterMaster Master;
+
+    private void Awake()
+    {
+        check.SetActive(false);
+    }
+
+    public void OnMouseDown()
+    {
+
+        if (Master.CheckAnswers(current.tag))
+        {
+            if (current.tag == "ElectricBox")
+            {
+                Master.stepOne = true;
+            }
+            else if (current.tag == "WaterPipe")
+            {
+                Master.stepTwo = true;
+            }
+            else if (current.tag == "AirPipe")
+            {
+                Master.stepThree = true;
+            }
+            else if (current.tag == "WaterSpout")
+            {
+                Master.stepFour = true;
+            }
+            Debug.Log("CHECKED OFF " + current.tag);
+            check.SetActive(true);
+
+        }
+        else
+        {
+            Master.resetSteps();
+            Debug.Log("Wrong Step restart");
+        }
+       
+    }
+}
--- Assets/Scripts/WaterHeaterMiniGame/WaterHeaterMaster.cs
+++ Assets/Scripts/WaterHeaterMiniGame/WaterHeaterMaster.cs
@@ -1,18 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class WaterHeaterMaster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Win;
+    public GameObject Wrong;
+
+    public UnityAction OnWin;
+    public UnityAction OnExit;
+
+    public GameObject ElectricBox;
+    public GameObject WaterPipe;
+    public GameObject AirPipe;
+    public GameObject WaterSpout;
+
+    public bool stepOne;
+    public bool stepTwo;
+    public bool stepThree;
+    public bool stepFour;
+
+    private void Start()
     {
-        
+        resetSteps();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool CheckAnswers(string tag)
     {
-        
+        if ((tag == "ElectricBox" && !stepOne && !stepTwo && !stepThree && !stepFour )
+        || (tag == "WaterPipe" && stepOne && !stepTwo && !stepThree && !stepFour)
+        || (tag == "AirPipe" && stepOne && stepTwo && !stepThree && !stepFour)
+        || (tag == "WaterSpout" && stepOne && stepTwo && stepThree && !stepFour))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void resetSteps()
+    {
+        stepOne = false;
+        stepTwo = false;
+        stepThree = false;
+        stepFour = false;
+        ElectricBox.SetActive(false);
+        WaterPipe.SetActive(false);
+        AirPipe.SetActive(false);
+        WaterSpout.SetActive(false);
+    }
+
+    public void Leave()
+    {
+        OnExit.Invoke();
+    }
+
+    public IEnumerable TryAgain()
+    {
+        Wrong.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        Wrong.SetActive(false);
+    }
+
+    public void WinLeave()
+    {
+        OnWin.Invoke();
     }
 }
