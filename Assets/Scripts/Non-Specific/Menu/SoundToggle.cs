11efb02e3f154f59266d8f9cbbbbf009d8a8c1ba
Blythe <blytheballesteros@gmail.com>
2020-07-01 13:02:03 -1000

    added script

---
+++ Assets/Scripts/Non-Specific/Menu/SoundToggle.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundToggle : MonoBehaviour
+{
+    public bool isOn;
+
+    public GameObject handle;
+    private RectTransform handleTransform;
+    private float handleSize;
+    public RectTransform toggle;
+    private float onPosX;
+    private float offPosX;
+    private float handleOffset = 75;
+
+    public GameObject onIcon;
+    public GameObject offIcon;
+
+    public GameObject audio;
+
+    public void Awake()
+    {
+        handleTransform = handle.GetComponent<RectTransform>();
+        handleSize = handleTransform.sizeDelta.x;
+        float toggleSizeX = toggle.sizeDelta.x;
+        onPosX = (toggleSizeX / 2) - (handleSize / 2) - handleOffset;
+        offPosX = onPosX * -1;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isOn)
+        {
+            handleTransform.localPosition = new Vector3(onPosX, 0, 0);
+            onIcon.gameObject.SetActive(true);
+            offIcon.gameObject.SetActive(false);
+            audio.SetActive(true);
+        }
+        else
+        {
+            handleTransform.localPosition = new Vector3(offPosX, 0, 0);
+            onIcon.gameObject.SetActive(false);
+            offIcon.gameObject.SetActive(true);
+            audio.SetActive(false);
+        }
+    }
+
+    public void StartToggle()
+    {
+        //if (!onIcon.activeSelf || !offIcon.activeSelf)
+        //{
+        //    onIcon.gameObject.SetActive(true);
+        //    offIcon.gameObject.SetActive(false);
+        //}
+        if (isOn)
+        {
+            Debug.Log("music off");
+            handleTransform.localPosition = SmoothlyMove(onPosX, offPosX);
+            onIcon.gameObject.SetActive(false);
+            offIcon.gameObject.SetActive(true);
+            audio.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("music on");
+            handleTransform.localPosition = SmoothlyMove(offPosX, onPosX);
+            onIcon.gameObject.SetActive(true);
+            offIcon.gameObject.SetActive(false);
+            audio.SetActive(true);
+        }
+    }
+
+    private Vector3 SmoothlyMove(float startPosX, float endPosX)
+    {
+        Vector3 position = new Vector3(Mathf.Lerp(startPosX, endPosX, 1), 0, 0);
+        switch (isOn)
+        {
+            case true:
+                isOn = false;
+                break;
+
+            case false:
+                isOn = true;
+                break;
+        }
+        return position;
+    }
+}
