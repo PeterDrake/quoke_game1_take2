5faf7cbb7d8614350481685b3cd13cec5c2d0b96
Lanaparezanin <lanaparezanin@lclark.edu>
2020-07-02 18:14:39 +0200

    Removed use button

--- Assets/Scripts/PitLatrineMiniGame/MiniGameMasterPitLatrine.cs
+++ Assets/Scripts/PitLatrineMiniGame/MiniGameMasterPitLatrine.cs
@@ -6,18 +6,19 @@ using UnityEngine.UI;
 public class MiniGameMasterPitLatrine : MonoBehaviour
 {
     public GameObject Camera;
-    public bool UseClicked;
+
+    //public bool UseClicked;
     private bool check;
 
     public Button PlywoodButton;
     public Button TarpButton;
     public GameObject PButton;
     public GameObject TButton;
-    
+
     public UnityAction OnWin;
     public UnityAction OnExit;
     public GameObject Win;
-    
+
     public GameObject S1Folder;
     public GameObject S2Folder;
     public GameObject S1Pit1;
@@ -29,23 +30,23 @@ public class MiniGameMasterPitLatrine : MonoBehaviour
     public GameObject Shovel2;
     public GameObject Tarp;
     public GameObject Plywood;
-    
+
     public GameObject ErrorScreen;
     public GameObject TryHighGround;
     public GameObject ErosionScreen;
     public GameObject WinScreen;
     public GameObject Depth1;
     public GameObject Depth2; //Depths 3 and 4 are regulated in MoveOtherShovel script
-    
-    public GameObject Use;
-    public GameObject Dig;    //Button for S1
-    public GameObject Dig2;    //Button for S2
+
+    //public GameObject Use;
+    public GameObject Dig; //Button for S1
+    public GameObject Dig2; //Button for S2
 
     public void Start()
     {
         PlywoodButton.GetComponent<Button>().interactable = false;
         TarpButton.GetComponent<Button>().interactable = false;
-        
+
         if (S1Folder.activeSelf)
         {
             Camera.transform.position = new Vector3(72.409f, 1.763f, -139.003f);
@@ -64,7 +65,7 @@ public class MiniGameMasterPitLatrine : MonoBehaviour
             Dig2.SetActive(true);
         }
     }
-    
+
     public void Update()
     {
         if (S1Folder.activeSelf)
@@ -78,15 +79,15 @@ public class MiniGameMasterPitLatrine : MonoBehaviour
             {
                 Depth2.SetActive(true);
             }
-            
-            if (S1Pit2.activeSelf == false && UseClicked)
+
+            /*if (S1Pit2.activeSelf == false && UseClicked)
             {
                 StartCoroutine(nameof(TooShallow));
-            }
+            }*/
 
             if (Water.activeSelf)
             {
-                Use.SetActive(false);
+                //Use.SetActive(false);
                 Dig.SetActive(false);
                 PButton.SetActive(false);
                 TButton.SetActive(false);
@@ -96,16 +97,17 @@ public class MiniGameMasterPitLatrine : MonoBehaviour
 
         else
         {
-            if (!S2Pit2.activeSelf && !S2Pit3.activeSelf && UseClicked)
+            /*if (!S2Pit2.activeSelf && !S2Pit3.activeSelf && UseClicked)
             {
                 StartCoroutine(nameof(TooShallow));
-            }
+            }*/
 
-            if (Tarp.activeSelf && UseClicked)
+            if (Tarp.activeSelf) //&& UseClicked)
             {
                 //Use.SetActive(false);
                 //Dig2.SetActive(false);
-                WinScreen.SetActive(true);
+                StartCoroutine(nameof(Winning));
+                //WinScreen.SetActive(true);
             }
 
             if (Plywood.activeSelf)
@@ -121,7 +123,7 @@ public class MiniGameMasterPitLatrine : MonoBehaviour
                     TarpButton.GetComponent<Button>().interactable = false;
                 }
             }
-            
+
             if (S2Pit2.activeSelf && !S2Pit3.activeSelf)
             {
                 PlywoodButton.GetComponent<Button>().interactable = true;
@@ -139,19 +141,26 @@ public class MiniGameMasterPitLatrine : MonoBehaviour
             }
         }
     }
-    
-    private IEnumerator TooShallow()
+
+    /*private IEnumerator TooShallow()
     {
         ErrorScreen.SetActive(true);
         yield return new WaitForSeconds(4f);
         ErrorScreen.SetActive(false);
         UseClicked = false;
+    }*/
+
+    private IEnumerator Winning()
+    {
+        yield return new WaitForSeconds(1f);
+        WinScreen.SetActive(true);
+        
     }
 
     private IEnumerator TooDeep()
     {
         Dig2.SetActive(false);
-        Use.SetActive(false);
+        //Use.SetActive(false);
         PButton.SetActive(false);
         TButton.SetActive(false);
         
@@ -160,7 +169,7 @@ public class MiniGameMasterPitLatrine : MonoBehaviour
         yield return new WaitForSeconds(3f); 
         ErosionScreen.SetActive(false);
         Dig2.SetActive(true);
-        Use.SetActive(true);
+        //Use.SetActive(true);
         PButton.SetActive(true);
         TButton.SetActive(true);
         PlywoodButton.GetComponent<Button>().interactable = false;
@@ -168,14 +177,14 @@ public class MiniGameMasterPitLatrine : MonoBehaviour
 
     private void UseIsClicked()
     {
-        UseClicked = true;
+        //UseClicked = true;
     }
     
     private IEnumerator TryElsewhere()
     {
         yield return new WaitForSeconds(3f);
         TryHighGround.SetActive(true);
-        UseClicked = false;
+        //UseClicked = false;
     }
     
     public void Leave()
