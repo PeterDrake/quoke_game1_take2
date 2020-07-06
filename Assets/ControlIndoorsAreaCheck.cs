cd3c711b4d8aabb08a46281affa742f4d9c5be2a
Lanaparezanin <lanaparezanin@lclark.edu>
2020-07-06 22:32:15 +0200

    Explosion during earthquake + doors is fixed

---
+++ Assets/ControlIndoorsAreaCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlIndoorsAreaCheck : MonoBehaviour
+{
+    public GameObject InsideCheck;
+    public GameObject DeathScreen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InsideCheck.SetActive(false);
+        StartCoroutine(nameof(Wait));
+    }
+
+    void Update()
+    {
+        if (DeathScreen.activeSelf)
+        {
+            InsideCheck.SetActive(false);
+        }
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(10f);
+        InsideCheck.SetActive(true);
+    }
+    
+}
