2826d9c20eb2a73676f1913a9f9aabaf73b083fe
Lanaparezanin <lanaparezanin@lclark.edu>
2020-07-10 17:50:28 +0200

    Mo kind of follows Zelda, navmesh baked

---
+++ Assets/SC_NPCFollowsPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SC_NPCFollowsPC : MonoBehaviour
+{
+    //Transform that NPC has to follow
+    public Transform transformToFollow;
+    //NavMesh Agent variable
+    NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //NavMeshAgent.Warp
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(transformToFollow.position);
+    }
+
+}
