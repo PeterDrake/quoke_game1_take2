4c515aa61cf952f1e9e9a607f7a05d4c2f7ab081
Lanaparezanin <lanaparezanin@lclark.edu>
2020-07-10 19:57:17 +0200

    Polished a script

--- Assets/SC_NPCFollowsPC.cs
+++ Assets/SC_NPCFollowsPC.cs
@@ -7,7 +7,6 @@ public class SC_NPCFollowsPC : MonoBehaviour
 {
     //Transform that NPC has to follow
     public Transform transformToFollow;
-    public GameObject Player;
     //NavMesh Agent variable
     NavMeshAgent agent;
     
@@ -15,25 +14,12 @@ public class SC_NPCFollowsPC : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        //NavMeshAgent.Warp
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Mathf.Abs(Player.transform.position.x - agent.transform.position.x) > 5 || Mathf.Abs(Player.transform.position.z - agent.transform.position.z) > 5)
-        //{
-            //agent.SetDestination(new Vector3(transformToFollow.position.x - 1, transformToFollow.position.y, transformToFollow.position.z-1));
-        //}
-
         agent.SetDestination(transformToFollow.position);
-
-        //agent.SetDestination(new Vector3(transformToFollow.position.x - 1, transformToFollow.position.y, transformToFollow.position.z-1));
-        
-        /*else
-        {
-            agent.GetComponent<Animator>().Play("idle");
-        }*/
     }
 
 }
