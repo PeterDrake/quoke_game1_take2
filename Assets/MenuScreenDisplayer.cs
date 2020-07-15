7d95cfa2daceed121dc89316af57acd6da3050cb
Annabel Paris <aparis@lclark.edu>
2020-07-15 14:03:48 -0700

    added pause menu to main menu

---
+++ Assets/MenuScreenDisplayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScreenDisplayer : UIElement
+{
+    private GameObject toggler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggler = GameObject.Find("EscapeClosed");
+        UIManager.Instance.Initialize(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Open()
+    {
+        toggler.SetActive(true);
+    }
+
+    public override void Close()
+    {
+        toggler.SetActive(false);
+    }
+}
