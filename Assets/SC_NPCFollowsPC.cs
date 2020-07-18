4fafc57e523828712d095ebef63a9f67d643e9a1
Lanaparezanin <lanaparezanin@lclark.edu>
2020-07-18 13:28:52 +0200

    Organized SC_NPCFollowsPC script

--- Assets/SC_NPCFollowsPC.cs
+++ Assets/SC_NPCFollowsPC.cs
@@ -9,11 +9,10 @@ public class SC_NPCFollowsPC : MonoBehaviour
     public Transform transformToFollow;
     //NavMesh Agent variable
     NavMeshAgent agent;
+    
     public GameObject Player;
     public GameObject NPC;
-    int check = 0;
     private Animator animator;
-    private bool walks;
     
     
     // Start is called before the first frame update
@@ -27,24 +26,19 @@ public class SC_NPCFollowsPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log(Player.transform.position.x + " and " + NPC.transform.position.x);
-            Debug.Log(Player.transform.position.z + " and " + NPC.transform.position.z);
-        }
-        //NPC.GetComponent<Animator>().Play("Walking");
         transform.LookAt(Player.transform);
+        
         if (Mathf.Abs(Player.transform.position.x - NPC.transform.position.x) > 2.8  ||
             Mathf.Abs(Player.transform.position.z - NPC.transform.position.z) > 2.8)
         {
-            Debug.Log("Walking");
             animator.SetBool("isWalking", true);
         }
+        
         else
         {
-            Debug.Log("Idle");
             animator.SetBool("isWalking", false);
         }
+        
         agent.SetDestination(transformToFollow.position);
     }
 
