45868dbf33a8227a6bfdd4678f8c16d0856ac497
Lanaparezanin <lanaparezanin@lclark.edu>
2020-07-20 12:19:11 +0200

    Barrel doesn't go through the floor anymore

--- Assets/Scripts/WaterBucketsMiniGame/DragBarrel.cs
+++ Assets/Scripts/WaterBucketsMiniGame/DragBarrel.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 
 public class DragBarrel : MonoBehaviour
 {
-    Vector3 startPos;
+     Vector3 startPos;
      Vector3 dist;
 
     void OnMouseDown()
-    {
+    {   
         startPos = Camera.main.WorldToScreenPoint(transform.position);
-        dist = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, startPos.z));
+        dist = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0.1f, startPos.z));
     }
 
     void OnMouseDrag()
     {
-        Vector3 lastPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, startPos.z);
+        Vector3 lastPos = new Vector3(Input.mousePosition.x, 0, startPos.z);
         transform.position = Camera.main.ScreenToWorldPoint(lastPos) + dist;
     }
+    
 }
