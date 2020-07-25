1988b21d21b43af134f9dedbcaf4db185be92461
Annabel Paris <aparis@lclark.edu>
2020-07-24 17:02:31 -0700

    sprites added and set up to follow objects if necessary

---
+++ Assets/Scripts/Non-Specific/FlatFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlatFollow : MonoBehaviour
+{
+    public GameObject following;
+    public bool mobile;
+
+    private Transform location;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        location = GetComponent<Transform>();
+        location.transform.position = new Vector3
+            (following.transform.position.x, 14, following.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mobile)
+        {
+            StartCoroutine(follow());
+        }
+    }
+
+    private IEnumerator follow()
+    {
+        while (true)
+        {
+            location.transform.position = new Vector3
+                (following.transform.position.x, 14, following.transform.position.z);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
