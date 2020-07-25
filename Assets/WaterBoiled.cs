82324d6f16195029e63a3371a9c580e8a1e9f6a5
Lanaparezanin <lanaparezanin@lclark.edu>
2020-07-25 21:45:11 +0200

    Water can boil

--- Assets/WaterBoiled.cs
+++ Assets/WaterBoiled.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class WaterBoiled : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject Barrel;
+    public GameObject Steam;
+    public GameObject Fire;
+    public GameObject Fire2;
+    
     void Update()
     {
-        
+        if (Barrel.activeSelf)
+        {
+            StartCoroutine(nameof(DestroyFire));
+        }
     }
+    
+    private IEnumerator DestroyFire()
+    {
+        yield return new WaitForSeconds(3f);
+        Steam.SetActive(true);
+        Fire.SetActive(false);
+        yield return new WaitForSeconds(2f);
+        Fire2.SetActive(false);
+    }
+    
 }
