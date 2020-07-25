4b443843f18a5fcead6c10706b312268e8bdb9fe
Lanaparezanin <lanaparezanin@lclark.edu>
2020-07-25 20:57:29 +0200

    Water can be successfully built

---
+++ Assets/EvokeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvokeController : MonoBehaviour
+{
+    public GameObject Controller;
+    
+    void Start()
+    {
+        StartCoroutine(nameof(Evoke));
+    }
+
+    private IEnumerator Evoke()
+    {
+        yield return new WaitForSeconds(1f);
+        Controller.SetActive(true);
+    }
+}
