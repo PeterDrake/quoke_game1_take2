399d057b2d3c9cc63b2b504a45ece0eedc54caff
Annabel Paris <aparis@lclark.edu>
2020-07-28 16:16:15 -0700

    make dialogue outcomes for moretosay and nothingtosay and put in dialogue L3

---
+++ Assets/Scripts/Non-Specific/Dialogue/Outcomes/ChangePointersOrAlerts.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Objective Outcome", menuName = "Dialogue/Outcomes/ChangePointersOrAlerts")]
+public class ChangePointersOrAlerts : DialogueOutcome
+{
+    public string ObjectiveName;
+
+    public string targetGameObject;
+
+    public bool makeAppear;
+
+    public override void DoOutcome(ref NPC n)
+    {
+        if (makeAppear)
+        {
+            GameObject.Find(targetGameObject).GetComponent<FlatFollow>().appear();
+        }
+        else 
+        { 
+            GameObject.Find(targetGameObject).GetComponent<FlatFollow>().disappear(); 
+        }
+    }
+}
