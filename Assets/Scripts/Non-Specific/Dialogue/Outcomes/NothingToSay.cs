d06db165d67047ded49c130d651f3987db55a72f
Annabel Paris <aparis@lclark.edu>
2020-07-27 17:10:27 -0700

    dialogue outcome turns off pointer in L1

---
+++ Assets/Scripts/Non-Specific/Dialogue/Outcomes/NothingToSay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Objective Outcome", menuName = "Dialogue/Outcomes/NothingToSay")]
+public class NothingToSay : DialogueOutcome
+{
+    public string ObjectiveName;
+
+    public string targetGameObject;
+
+    public override void DoOutcome(ref NPC n)
+    {
+        GameObject.Find(targetGameObject).GetComponent<FlatFollow>().disappear();
+    }
+}
