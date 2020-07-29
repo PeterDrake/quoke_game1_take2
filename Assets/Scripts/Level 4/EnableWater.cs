83dd0e157092ce3c713d8c6a478a3de307d5cd64
Lanaparezanin <lanaparezanin@lclark.edu>
2020-07-29 13:16:09 +0200

    Fixed the banners

--- Assets/Scripts/Level 4/EnableWater.cs
+++ Assets/Scripts/Level 4/EnableWater.cs
@@ -6,12 +6,13 @@ public class EnableWater : MonoBehaviour
 {
     public GameObject Water;
     public GameObject Water1;
+    public InformationCanvas _canvas;
+    public string words;
     
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(nameof(EnableTheWater));
-        //Water.SetActive(true);
     }
     
     private IEnumerator EnableTheWater()
@@ -19,5 +20,6 @@ public class EnableWater : MonoBehaviour
         yield return new WaitForSeconds(7f);
         Water1.SetActive(false);
         Water.SetActive(true);
+        _canvas.ChangeText(words);
     }
 }
