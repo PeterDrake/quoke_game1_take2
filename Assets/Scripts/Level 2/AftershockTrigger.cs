b6a0248bca043a5d03862790adff6123d647de3e
Annabel Paris <aparis@lclark.edu>
2020-07-30 14:16:12 -0700

    change banner on exit to talk to survivors

--- Assets/Scripts/Level 2/AftershockTrigger.cs
+++ Assets/Scripts/Level 2/AftershockTrigger.cs
@@ -7,6 +7,7 @@ public class AftershockTrigger : MonoBehaviour
 {
     private bool aftershock = false;
 
+
     // Attached to a game object along with a collider to detect when the player last left the house
     public InformationCanvas _canvas;
     private void OnTriggerExit(Collider other)
@@ -14,7 +15,7 @@ public class AftershockTrigger : MonoBehaviour
         Debug.Log("CHANGE TO TWO");
         QuakeManager.Instance.TriggerCountdown(2f);
         aftershock = true;
-        _canvas.ChangeText("Have a look around...");
+        _canvas.ChangeText("Talk to survivors");
         GameObject.Find("TablePointer").GetComponent<FlatFollow>().disappear();
         //Logger.Instance.Log("Player has left the house");
         LogToServer logger = GameObject.Find("Logger").GetComponent<LogToServer>();
