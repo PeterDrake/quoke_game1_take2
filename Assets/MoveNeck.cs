0e7a8b20a288a4bbb6453a74eacb7fc8ab82059e
Lanaparezanin <lanaparezanin@lclark.edu>
2020-08-01 15:47:18 +0200

    Corgi trailer scenery

---
+++ Assets/MoveNeck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveNeck : MonoBehaviour
+{
+
+    public GameObject Neck; // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(nameof(MoveTheNeck));
+    }
+
+    private IEnumerator MoveTheNeck()
+    {
+        yield return new WaitForSeconds(5f);
+        Neck.transform.Rotate(0, 34, 0);
+    }
+}
