cef77076c9260f7d81d3f368733b9127f9c54b79
Blythe <blytheballesteros@gmail.com>
2020-08-03 14:19:13 -1000

    did something so Mo doesnt get stuck on objects anymore

---
+++ Assets/Scripts/Level 4/FollowPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    //You may consider adding a rigid body to the zombie for accurate physics simulation
+    private GameObject wayPoint;
+    private Vector3 wayPointPos;
+    public float offset = 5f;
+    //This will be the zombie's speed. Adjust as necessary.
+    private float speed = 2.0f;
+    NavMeshAgent nav;
+    Animator animator;
+    GameObject Player;
+    void Start()
+    {
+        //At the start of the game, the zombies will find the gameobject called wayPoint.
+        wayPoint = GameObject.Find("wayPoint");
+        nav = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        animator.SetBool("isWalking", true);
+        wayPointPos = new Vector3(wayPoint.transform.position.x, transform.position.y, wayPoint.transform.position.z + offset);
+        //Here, the zombie's will follow the waypoint.
+        transform.position = Vector3.MoveTowards(transform.position, wayPointPos, Time.deltaTime);
+        if (nav.remainingDistance < nav.stoppingDistance)
+        {
+            print("Stop walking");
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+}
