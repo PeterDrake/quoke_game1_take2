225504ba554a26e13c64724b07203ab132b6b9cd
Lanaparezanin <lanaparezanin@lclark.edu>
2020-08-04 19:45:33 +0200

    Tarp can be moved to the proper position

---
+++ Assets/Scripts/SaveCorgiMiniGame/SaveCorgiController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveCorgiController : MonoBehaviour
+{
+    private DragTarp script;
+
+    public GameObject Tarp;
+
+    void Start()
+    {
+        script = Tarp.GetComponent<DragTarp>();
+    }
+
+    void Update()
+    {
+        if (Tarp.transform.position.x < -116f)
+        {
+            Destroy(script);
+            //won
+        }
+    }
+    
+    //First, instructions appear
+    // Then, you move tarp for corgi to jump
+    // animation corgi doesn't want to jump
+    // hot dog Frank comes there
+    // corgi jumps
+    // win screen2
+}
