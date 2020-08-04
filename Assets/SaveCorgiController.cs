1eee9472000043d6164f23eb0d68f02d0cea3d4c
Lanaparezanin <lanaparezanin@lclark.edu>
2020-08-04 15:57:55 +0200

    Fixed a minor bug

---
+++ Assets/SaveCorgiController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveCorgiController : MonoBehaviour
+{
+    //First, instructions appear
+    // Then, you move tarp for corgi to jump
+    // animation corgi doesn't want to jump
+    // hot dog Frank comes there
+    // corgi jumps
+    // win screen2
+}
