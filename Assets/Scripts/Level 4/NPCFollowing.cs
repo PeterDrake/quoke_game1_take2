f69130bf41bb46e7dbdf287307d24a1c5439cf90
Blythe <blytheballesteros@gmail.com>
2020-08-04 13:44:12 -1000

    follows better

---
+++ Assets/Scripts/Level 4/NPCFollowing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCFollowing : MonoBehaviour
+{
+    public GameObject Leader;
+    private NavMeshAgent nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        HeadForDestination();
+    }
+
+    private void HeadForDestination()
+    {
+        Vector3 destination = Leader.transform.position;
+        nav.SetDestination(destination);
+    }
+}
