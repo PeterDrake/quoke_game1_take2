71792a3382ff8f0f323bc87146ab266fe757f417
Blythe <blytheballesteros@gmail.com>
2020-08-04 16:05:49 -1000

    always facing zelda

--- Assets/Scripts/Level 4/NPCFollowing.cs
+++ Assets/Scripts/Level 4/NPCFollowing.cs
@@ -22,17 +22,18 @@ public class NPCFollowing : MonoBehaviour
 
     private void Update()
     {
+
         float distance = Vector3.Distance(Leader.transform.position, transform.position);
 
         if (distance < nav.stoppingDistance)
         {
-            print("I GOT THERE");
             NPCanimator.SetBool("isWalking", false);
+            transform.LookAt(Leader.transform.position);
+
         }
         else
         {
             NPCanimator.SetBool("isWalking", true);
-            print("NOT THERE YET");
 
         }
         HeadForDestination();
@@ -43,7 +44,6 @@ public class NPCFollowing : MonoBehaviour
     {
         Vector3 destination = Leader.transform.position;
         //transform.LookAt(new Vector3(Leader.transform.position.x, transform.position.y, Leader.transform.position.z));
-        
         nav.SetDestination(destination);
     }
 
