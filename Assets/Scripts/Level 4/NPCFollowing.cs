038f5fb8c00d8599a26da81794106d8ae28e7d0f
Blythe <blytheballesteros@gmail.com>
2020-08-04 15:50:22 -1000

    animator works but drifts to end

--- Assets/Scripts/Level 4/NPCFollowing.cs
+++ Assets/Scripts/Level 4/NPCFollowing.cs
@@ -22,18 +22,28 @@ public class NPCFollowing : MonoBehaviour
 
     private void Update()
     {
-        //float distance = Vector3.Distance(Leader.transform.position, transform.position);
+        float distance = Vector3.Distance(Leader.transform.position, transform.position);
 
+        if (distance < nav.stoppingDistance)
+        {
+            print("I GOT THERE");
+            NPCanimator.SetBool("isWalking", false);
+        }
+        else
+        {
+            NPCanimator.SetBool("isWalking", true);
+            print("NOT THERE YET");
 
-            HeadForDestination();
-        
+        }
+        HeadForDestination();
 
     }
 
     private void HeadForDestination()
     {
         Vector3 destination = Leader.transform.position;
-        transform.LookAt(new Vector3(Leader.transform.position.x, transform.position.y, Leader.transform.position.z));
+        //transform.LookAt(new Vector3(Leader.transform.position.x, transform.position.y, Leader.transform.position.z));
+        
         nav.SetDestination(destination);
     }
 
