797a5282f5ab4d6f273bf54ca2a28b3b7f960f01
Annabel Paris <aparis@lclark.edu>
2020-08-05 13:41:15 -0700

    more setup progress for sound sliders

--- Assets/ChangeMusicSoundValues.cs
+++ Assets/ChangeMusicSoundValues.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class ChangeMusicSoundValues : MonoBehaviour
 {
+    private int musicVal;
+    private int sfxVal;
+
+    public GameObject mHandle;
+    public GameObject sHandle;
+
+    private RectTransform mHandleTransform;
+    private RectTransform sHandleTransform;
+    private float handleSize;
+
+    private RectTransform mToggleTransform;
+    private RectTransform sToggleTransform;
+
+    private GameObject musicAudio;
+    private GameObject sfxAudio;
+
     // Start is called before the first frame update
     void Start()
     {
+        musicVal = SavedData.musicInt;
+        sfxVal = SavedData.sfxInt;
+
+        mHandleTransform = GameObject.Find("handle 1").GetComponent<RectTransform>();
+        sHandleTransform = GameObject.Find("handle 2").GetComponent<RectTransform>();
+        //handleSize = 35;
+
+        //mHandleTransform.x = musicVal - 50;
+        //sHandleTransform.x = sfxVal - 50;
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        musicVal = GameObject.Find("MusicSlider").GetComponent<Slider>().Value;
+        sfxVal = GameObject.Find("SFXSlider").GetComponent<Slider>().Value;
+
+
+        SavedData.musicInt = musicVal;
+        SavedData.sfxInt = sfxVal;
+    }
+
+    public void ChangeMusicValue(int val)
+    {
+
     }
 }
