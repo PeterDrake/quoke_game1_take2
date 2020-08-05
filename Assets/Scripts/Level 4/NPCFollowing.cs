2d8a2ebd8fe2fc726b55c6b0baf8fc8033ad0b76
Blythe <blytheballesteros@gmail.com>
2020-08-04 19:27:41 -1000

    mo now walks himself to the shelter after talking to maria

--- Assets/Scripts/Level 4/NPCFollowing.cs
+++ Assets/Scripts/Level 4/NPCFollowing.cs
@@ -10,6 +10,8 @@ public class NPCFollowing : MonoBehaviour
     private NavMeshAgent nav;
     private Animator NPCanimator;
     private Animator Leaderanimator;
+    public bool shelter;
+    private bool inShelter;
 
     // Start is called before the first frame update
     void Start()
@@ -17,44 +19,68 @@ public class NPCFollowing : MonoBehaviour
         nav = GetComponent<NavMeshAgent>();
         NPCanimator = NPC.GetComponent<Animator>();
         Leaderanimator = Leader.GetComponent<Animator>();
-        HeadForDestination();
+        shelter = false;
+        inShelter = false;
     }
 
     private void Update()
     {
-
-        float distance = Vector3.Distance(Leader.transform.position, transform.position);
-        if (distance > 10)
+        if (shelter)
         {
-            NPCanimator.SetBool("isWalking", false);
-            NPCanimator.SetBool("isRunning", true);
-            nav.speed = 6;
-        }
-        if (distance <= 10)
-        {
+            print("GOING TO SHELTER");
+            Vector3 destination = GameObject.Find("SchoolDoorStep").transform.position;
+            float distance = Vector3.Distance(destination, transform.position);
+            nav.stoppingDistance = 2f;
             NPCanimator.SetBool("isWalking", true);
-            NPCanimator.SetBool("isRunning", false);
-            nav.speed = 3;
-        }
-        if (distance < nav.stoppingDistance)
-        {
-            NPCanimator.SetBool("isWalking", false);
-            transform.LookAt(new Vector3(Leader.transform.position.x, transform.position.y, Leader.transform.position.z));
 
+
+            if (distance <= nav.stoppingDistance)
+            {
+                print("in the shelter I MAAADDDEE IT ");
+                GameObject.Find("Mo").SetActive(false);
+                GameObject.Find("MoPointer").SetActive(false);
+                GameObject.Find("MoAlert").SetActive(false);
+                this.enabled = false;
+            }
+
+            HeadForDestination(destination);
         }
+
         else
         {
-            NPCanimator.SetBool("isWalking", true);
+            print("FOLLOWING ZELDA");
+            float distance = Vector3.Distance(Leader.transform.position, transform.position);
+            Vector3 destination = Leader.transform.position;
+            if (distance > 10)
+            {
+                print("TOOO FARRRR!!! ILL RUN");
+                NPCanimator.SetBool("isWalking", false);
+                NPCanimator.SetBool("isRunning", true);
+                nav.speed = 6;
+            }
+            if (distance <= 10)
+            {
+                print("ILL WALK THERE DONT WORRY");
+                NPCanimator.SetBool("isWalking", true);
+                NPCanimator.SetBool("isRunning", false);
+                nav.speed = 3;
+            }
+            if (distance <= nav.stoppingDistance)
+            {
+                NPCanimator.SetBool("isWalking", false);
+            }
+            else
+            {
+                NPCanimator.SetBool("isWalking", true);
 
+            }
+
+            HeadForDestination(destination);
         }
-        HeadForDestination();
-
     }
 
-    private void HeadForDestination()
+    private void HeadForDestination(Vector3 destination)
     {
-        Vector3 destination = Leader.transform.position;
-        //transform.LookAt(new Vector3(Leader.transform.position.x, transform.position.y, Leader.transform.position.z));
         nav.SetDestination(destination);
     }
 
