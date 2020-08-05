117bb203e23776b8df21770d887f9c94997479c0
Ela Pencl <elapencl@clark.edu>
2020-08-05 09:17:24 -0700

    able to talk to ahmad and added script

---
+++ Assets/Scripts/Non-Specific/Dialogue/Outcomes/GameObjectAppear.cs
@@ -0,0 +1,106 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Objective Outcome", menuName = "Dialogue/Outcomes/IsObjectiveSatisfied")]
+public class IsObjectiveSatisfied : DialogueOutcome
+{
+    public GameObject WhatNeedsToAppear;
+    public SatisfyObjective WhatNeedsToBeSatisfied;
+
+    public override void DoOutcome(ref NPC n)
+    {
+        if (WhatNeedsToBeSatisfied)
+        {
+            WhatNeedsToAppear.SetActive(true);
+        }
+        else
+        {
+            WhatNeedsToAppear.SetActive(false);
+        }
+    }
+}*/
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Objective Outcome", menuName = "Dialogue/Outcomes/IsObjectiveSatisfied")]
+public class IsObjectiveSatisfied : DialogueOutcome
+{
+    private GameObject WhatNeedsToAppear;
+    public SatisfyObjective WhatNeedsToBeSatisfied;
+
+    public override void DoOutcome(ref NPC n)
+    {
+        //WhatNeedsToAppear = GameObject.Find(targetGameObject);
+
+        if (WhatNeedsToBeSatisfied)
+        {
+            WhatNeedsToAppear.Find(targetGameObject).GetComponent<InteractWithMaria>().AhmadAppear();
+        }
+        else
+        {
+            WhatNeedsToAppear.Find(targetGameObject).GetComponent<InteractWithMaria>().AhmadDisappear();
+        }
+    }
+}*/
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Objective Outcome", menuName = "Dialogue/Outcomes/IsObjectiveSatisfied")]
+public class IsObjectiveSatisfied : DialogueOutcome
+{
+    public GameObject WhatNeedsToAppear;
+    
+
+    public override void DoOutcome(ref NPC n)
+    {
+        WhatNeedsToAppear.SetActive(true);  
+    }
+}*/
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Objective Outcome", menuName = "Dialogue/Outcomes/IsObjectiveSatisfied")]
+public class IsObjectiveSatisfied : DialogueOutcome
+{
+    public string WhatNeedsToAppear;
+    public SatisfyObjective WhatsSatisfied;
+
+
+    public override void DoOutcome(ref NPC n)
+    {
+        if (WhatsSatisfied)
+        {
+            GameObject.Find(WhatNeedsToAppear).SetActive(true);
+        }
+
+        else
+        {
+            GameObject.Find(WhatNeedsToAppear).SetActive(false);
+        }
+        
+    }
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New GameObject Addition", menuName = "Dialogue/Outcomes/IsObjectiveSatisfied")]
+public class IsObjectiveSatisfied : DialogueOutcome
+{
+    public string WhatNeedsToAppear;
+
+
+    public override void DoOutcome(ref NPC n)
+    {
+        GameObject.Find(WhatNeedsToAppear).SetActive(true);
+    }
+}
