a27edb7d3e22353c5edc11afce062b5fd924ad37
Blythe <blytheballesteros@gmail.com>
2020-08-06 13:22:53 -1000

    mo goes to shelter and waits before entering

--- Assets/Scripts/Level 4/NPCFollowing.cs
+++ Assets/Scripts/Level 4/NPCFollowing.cs
@@ -5,68 +5,80 @@ using UnityEngine.AI;
 
 public class NPCFollowing : MonoBehaviour
 {
-    //public GameObject Leader;
-    //public GameObject NPC;
-    //private NavMeshAgent nav;
-    //private Animator NPCanimator;
-    //private Animator Leaderanimator;
-    //public bool shelter;
-    //private bool inShelter;
-    //private float runAwayDistance;
-
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    nav = GetComponent<NavMeshAgent>();
-    //    NPCanimator = NPC.GetComponent<Animator>();
-    //    Leaderanimator = Leader.GetComponent<Animator>();
-    //    shelter = false;
-    //    inShelter = false;
-    //    runAwayDistance = 5f;
-        
-    //}
-
-
     public float runAwayDistance = 10;
     public GameObject targetGO;
     public GameObject NPC;
     private NavMeshAgent navMeshAgent;
     private Animator animator;
     private float last;
+    public bool shelter = false;
+    private int check = 20;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         animator = NPC.GetComponent<Animator>();
+
         last = 0f;
     }
     private void Update()
     {
-        Vector3 targetPosition = targetGO.transform.position;
-        float distanceToTarget = Vector3.Distance(transform.position, targetPosition);
-
-        if (distanceToTarget > runAwayDistance - 1 && distanceToTarget < runAwayDistance + 1 || last == distanceToTarget)
+        if (shelter)
         {
-            print(distanceToTarget + "im here");
-            animator.SetBool("isRunning", false);
-            animator.SetBool("isWalking", false);
-        }
-
-        else if (distanceToTarget < runAwayDistance - 1)
-        {
-            print("wayyyy close");
-            animator.SetBool("isRunning", false);
+            print("GOING TO SHELTER");
+            Vector3 destination = GameObject.Find("SchoolDoorStep").transform.position;
+            float distance = Vector3.Distance(destination, transform.position);
+            runAwayDistance = 2f;
+            navMeshAgent.speed = 3f;
             animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
+
+            if (check == 0)
+            {
+                print("in the shelter I MAAADDDEE IT ");
+                GameObject.Find("Mo").SetActive(false);
+                GameObject.Find("MoPointer").SetActive(false);
+                GameObject.Find("MoAlert").SetActive(false);
+                this.enabled = false;
+            }
+
+            if (distance <= 2.5)
+            {
+                animator.SetBool("isWalking", false);
+                check--;
+            }
+
+            FleeFromTarget(destination);
         }
+
 
         else
         {
-            animator.SetBool("isRunning", true);
-            animator.SetBool("isWalking", false);
-            print("running" + distanceToTarget);
+            Vector3 targetPosition = targetGO.transform.position;
+            float distanceToTarget = Vector3.Distance(transform.position, targetPosition);
+
+            if (distanceToTarget > runAwayDistance - 1 && distanceToTarget < runAwayDistance + 1 || last == distanceToTarget)
+            {
+                print(distanceToTarget + "im here");
+                animator.SetBool("isRunning", false);
+                animator.SetBool("isWalking", false);
+            }
+
+            else if (distanceToTarget < runAwayDistance - 1)
+            {
+                print("wayyyy close");
+                animator.SetBool("isRunning", false);
+                animator.SetBool("isWalking", true);
+            }
+            else
+            {
+                animator.SetBool("isRunning", true);
+                animator.SetBool("isWalking", false);
+                print("running" + distanceToTarget);
+            }
+            FleeFromTarget(targetPosition);
+            last = distanceToTarget;
         }
-        FleeFromTarget(targetPosition);
-        last = distanceToTarget;
     }
 
     private void FleeFromTarget(Vector3 targetPosition)
@@ -87,71 +99,6 @@ public class NPCFollowing : MonoBehaviour
         Vector3 runToPosition = targetPosition + (transform.forward * runAwayDistance);
         return runToPosition;
     }
+
+
 }
-
-
-//void Update()
-//{
-
-//if (shelter)
-//{
-//    print("GOING TO SHELTER");
-//    Vector3 destination = GameObject.Find("SchoolDoorStep").transform.position;
-//    float distance = Vector3.Distance(destination, transform.position);
-//    nav.stoppingDistance = 2f;
-//    NPCanimator.SetBool("isWalking", true);
-
-
-//    if (distance <= nav.stoppingDistance)
-//    {
-//        print("in the shelter I MAAADDDEE IT ");
-//        GameObject.Find("Mo").SetActive(false);
-//        GameObject.Find("MoPointer").SetActive(false);
-//        GameObject.Find("MoAlert").SetActive(false);
-//        this.enabled = false;
-//    }
-
-//    HeadForDestination(destination);
-//}
-
-//else
-//{
-//    print("FOLLOWING ZELDA");
-//    float distance = Vector3.Distance(Leader.transform.position, transform.position);
-//    Vector3 destination = Leader.transform.position + (transform.forward *  runAwayDistance);
-//if (distance > 10)
-//{
-//    print("TOOO FARRRR!!! ILL RUN");
-//    NPCanimator.SetBool("isWalking", false);
-//    NPCanimator.SetBool("isRunning", true);
-//    nav.speed = 6;
-//}
-//if (distance <= 10)
-//{
-//    print("ILL WALK THERE DONT WORRY");
-//    NPCanimator.SetBool("isWalking", true);
-//    NPCanimator.SetBool("isRunning", false);
-//    nav.speed = 3;
-//}
-//if (distance <= nav.stoppingDistance)
-//{
-//    NPCanimator.SetBool("isWalking", false);
-//}
-//else
-//{
-//    NPCanimator.SetBool("isWalking", true);
-
-//}
-
-//HeadForDestination(destination);
-//        }
-//    }
-
-//    private void HeadForDestination(Vector3 destination)
-//    {
-//        nav.SetDestination(destination);
-//    }
-
-
-
-//}
