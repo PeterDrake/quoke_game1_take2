28c4c2c23d1b8a6051df80c2f4ada64d29f2ca4f
Annabel Paris <aparis@lclark.edu>
2020-08-06 16:11:34 -0700

    the sliders slidegit status!

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string musicPref = "musicPref";
+    private static readonly string sfxPref = "sfxPref";
+
+    private int firstPlayInt;
+    public Slider musicSlider, sfxSlider;
+    private float musicFloat, sfxFloat;
+
+    void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+
+        if(firstPlayInt==0)
+        {
+            musicFloat = 0.25f;
+            sfxFloat = 0.5f;
+            musicSlider.value = musicFloat;
+            sfxSlider.value = sfxFloat;
+            PlayerPrefs.SetFloat(musicPref, musicFloat);
+            PlayerPrefs.SetFloat(sfxPref, sfxFloat);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+        else
+        {
+            musicFloat = PlayerPrefs.GetFloat(musicPref);
+            musicSlider.value = musicFloat;
+            sfxFloat = PlayerPrefs.GetFloat(sfxPref);
+            sfxSlider.value = sfxFloat;
+        }
+    }
+
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat(musicPref, musicSlider.value);
+        PlayerPrefs.SetFloat(sfxPref, sfxSlider.value);
+    }
+
+    void OnApplicationFocus(bool inFocus)
+    {
+        if(!inFocus)
+        {
+            SaveSoundSettings();
+        }
+    }
+}
