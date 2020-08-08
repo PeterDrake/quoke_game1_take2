f157e6b486ecef757e067c7de3c6569c5b70098b
Blythe <blytheballesteros@gmail.com>
2020-08-07 21:11:26 -1000

    Revert "merge"
    This reverts commit 2675368a5d29675434be7248ff3eea4e231a23e5, reversing
    changes made to 2733809e5b342a2dba672bba42969ee0f61fb3f8.

---
+++ Assets/Scripts/Non-Specific/Dialogue/Outcomes/FollowZelda.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Objective Outcome", menuName = "Dialogue/Outcomes/FollowZelda")]
+public class FollowZelda : DialogueOutcome
+{
+    public string NPCFollower;
+
+    public override void DoOutcome(ref NPC n)
+    {
+        GameObject.Find(NPCFollower + "Controller").GetComponent<NPCFollowing>().enabled = true;
+        GameObject.Find(NPCFollower + "Controller").GetComponent<InteractWithObject>().enabled = false;
+        GameObject.Find(NPCFollower + "Controller").GetComponent<SphereCollider>().enabled = false;
+    }
+}
