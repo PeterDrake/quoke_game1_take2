6ebb8261ffeba15276be4eac11b7aac24ea631c6
Blythe <blytheballesteros@gmail.com>
2020-08-08 11:50:13 -1000

    bruce follows zelda after talking

---
+++ Assets/Scripts/Non-Specific/Dialogue/Outcomes/FollowZelda.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "New Objective Outcome", menuName = "Dialogue/Outcomes/FollowZelda")]
+public class FollowZelda : DialogueOutcome
+{
+    public string NPCFollower;
+
+    public override void DoOutcome(ref NPC n)
+    {
+        GameObject.Find(NPCFollower + "Controller").GetComponent<NavMeshAgent>().enabled = true;
+        GameObject.Find(NPCFollower + "Controller").GetComponent<NPCFollowing>().enabled = true;
+        GameObject.Find(NPCFollower + "Controller").GetComponent<InteractWithObject>().enabled = false;
+        GameObject.Find(NPCFollower + "Controller").GetComponent<SphereCollider>().enabled = false;
+    }
+}
