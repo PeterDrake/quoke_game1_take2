8944775a63a83c4b3c01adad923e16426ce0dd10
Blythe <blytheballesteros@gmail.com>
2020-08-10 17:41:31 -1000

    working on building the composting toilet

---
+++ Assets/Scripts/Level 4/CompostingSwapText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompostingSwapText : MonoBehaviour
+{
+    private InteractWithObject _interact;
+    private void Start()
+    {
+        _interact = GetComponent<InteractWithObject>();
+    }
+
+    public void SwapTextBox()
+    {
+        _interact.SetInteractText("Press 'E' to build a box");
+    }
+
+    public void SwapTextCarbon()
+    {
+        _interact.SetInteractText("Press 'E' to add carbon material to box");
+    }
+    
+}
