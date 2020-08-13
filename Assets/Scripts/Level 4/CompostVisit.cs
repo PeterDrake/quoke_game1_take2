482582702ae737de68c8dd0768ed57abbcabe364
Blythe <blytheballesteros@gmail.com>
2020-08-13 12:36:18 -1000

    talk to bruce before building compost toilet

---
+++ Assets/Scripts/Level 4/CompostVisit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompostVisit : MonoBehaviour
+{
+    CompostingMaster master;
+    InteractWithObject _interact;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        master = GameObject.Find("CompostingMaster").GetComponent<CompostingMaster>();
+        _interact = GetComponent<InteractWithObject>();
+    }
+
+   public void Interaction()
+    {
+        if (Systems.Objectives.Check("CompostInstructions"))
+        {
+            master.AddCarbon();
+            master.BuildBox();
+        }
+        else
+        {
+            _interact.SetInteractText("Go talk to Bruce");
+
+        }
+    }
+}
