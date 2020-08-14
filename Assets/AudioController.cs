032ee9fdd6a0e3cfdb09964c383b3d89a11517af
Annabel Paris <aparis@lclark.edu>
2020-08-14 14:02:21 -0700

    fix audio for cutscenes

--- Assets/AudioController.cs
+++ Assets/AudioController.cs
@@ -9,13 +9,17 @@ public class AudioController : MonoBehaviour
 {
     private Slider musicSlide;
     private VideoPlayer video;
+    private float volume;
+    public bool halfVolume;
 
     // Start is called before the first frame update
     void Start()
     {
         musicSlide = GameObject.Find("AudioManager").GetComponent<AudioManager>().musicSlider;
         video = GetComponent<VideoPlayer>();
-        video.SetDirectAudioVolume(0, musicSlide.value);
+        if (halfVolume) { volume = musicSlide.value * 0.5f; }
+        else { volume = musicSlide.value; }
+        video.SetDirectAudioVolume(0, volume);
     }
 
     // Update is called once per frame
