49e340e5e653db0419278117250ce80720146af4
Blythe <blytheballesteros@gmail.com>
2020-09-08 13:12:00 -1000

    selection on main menu

--- Assets/Scripts/MenuScreenDisplayer.cs
+++ Assets/Scripts/MenuScreenDisplayer.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MenuScreenDisplayer : UIElement
 {
+    public GameObject level1;
+    public GameObject level2;
+    public GameObject level3;
+    public GameObject level4;
+    public GameObject post;
     private GameObject toggler;
     private bool musicIsOff;
     private bool sfxIsOff;
@@ -12,6 +19,12 @@ public class MenuScreenDisplayer : UIElement
     {
         toggler = GameObject.Find("EscapeClosed");
         UIManager.Instance.Initialize(this);
+        EventSystem.current.SetSelectedGameObject(level1);
+        //level1.GetComponent<Button>().OnSubmit(null);
+        //level2.GetComponent<Button>().OnSubmit(null);
+        //level3.GetComponent<Button>().OnSubmit(null);
+        //level4.GetComponent<Button>().OnSubmit(null);
+        //post.GetComponent<Button>().OnSubmit(null);
 
         /*print("EscapeClosed start method");
 
