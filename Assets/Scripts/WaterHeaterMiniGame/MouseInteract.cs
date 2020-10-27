a6785b80e6a4d2d3d07a9c341a3f05bd1fe30020
Blythe <blytheballesteros@gmail.com>
2020-10-27 13:10:52 -1000

    made hang tag more noticable

--- Assets/Scripts/WaterHeaterMiniGame/MouseInteract.cs
+++ Assets/Scripts/WaterHeaterMiniGame/MouseInteract.cs
@@ -6,11 +6,11 @@ public class MouseInteract: MonoBehaviour
 {
     public bool coroutineAllowed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        coroutineAllowed = false;
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    coroutineAllowed = true;
+    //}
 
     private void OnMouseOver()
     {
