c86f61a8a9c0c9091e30d40667cd1cd3b46f80cf
Blythe <blytheballesteros@gmail.com>
2020-11-13 13:49:11 -1000

    L1 works better now

--- Assets/Scripts/Non-Specific/HintController.cs
+++ Assets/Scripts/Non-Specific/HintController.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 
 public class HintController : MonoBehaviour
 {
-    public GameObject interactables;
+    public GameObject[] tasks;
     public Text hintText;
+
+    private int check = 0;
     
 
     // Start is called before the first frame update
@@ -15,23 +17,97 @@ public class HintController : MonoBehaviour
         print("HINTS ARE " + SavedData.hints);
         if (SavedData.hints)
         {
-            interactables.SetActive(true);
             hintText.text = "Hints ON";
         }
         else
         {
-            interactables.SetActive(false);
             hintText.text = "Hints OFF";
+        }
+        print("starting CHECKLIST");
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            print("setting tasks #" + i);
+            if (i == 0)
+            {
+                tasks[0].SetActive(true);
+            }
+            else
+            {
+                tasks[i].SetActive(false);
+            }
+
+            foreach(Transform child in tasks[i].transform)
+            {
+                if (SavedData.hints)
+                {
+                    print("task active = " + child.name);
+                    child.GetComponent<FlatFollow>().appear();
+                }
+                else
+                {
+                    print("task NOT active = " + child.name);
+                    child.GetComponent<FlatFollow>().disappear();
+                }
+            }
         }
     }
 
     public void TurnHintsOn()
     {
         hintText.text = "Hints ON";
-        interactables.SetActive(true);
         SavedData.hints = true;
         print("HINTS ARE " + SavedData.hints);
+        print("CHECKLIST UPDATED");
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            if (tasks[i].activeSelf)
+            {
+                foreach(Transform child in tasks[i].transform)
+                {
+                    print("Task turned active = " + child.name);
+                    child.GetComponent<FlatFollow>().appear();
+                }
+            }
+            else
+            {
+                foreach (Transform child in tasks[i].transform)
+                {
+                    print("Task NOT active = " + child.name);
+                    child.GetComponent<FlatFollow>().disappear();
+                }
+            }
+        }
 
+    }
+
+    public void NextTask()
+    {
+        check++;
+        print("next task is #" + check);
+        if (check> tasks.Length) { return;  }
+        tasks[check].SetActive(true);
+        foreach(Transform child in tasks[check].transform)
+        {
+            print(child.name + "is the next task");
+            if (SavedData.hints)
+            {
+                child.GetComponent<FlatFollow>().appear();
+            }
+            else
+            {
+                child.GetComponent<FlatFollow>().disappear();
+            }
+        }
+    }
+
+    public void TaskCompleted(GameObject turnoff)
+    {
+        print("TURNING " + turnoff.name + " OFF");
+        foreach(Transform child in turnoff.transform)
+        {
+            child.GetComponent<FlatFollow>().disappear();
+        }
+        turnoff.SetActive(false);
     }
 
 }
