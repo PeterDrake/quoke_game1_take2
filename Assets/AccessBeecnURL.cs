9a90c59e0bedcae2bb7021366e1a5cb4804efd03
Lanaparezanin <58230697+Lanaparezanin@users.noreply.github.com>
2020-11-14 16:00:27 -0800

    Access training buttons work

---
+++ Assets/AccessBeecnURL.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessBeecnURL : MonoBehaviour
+{
+    
+
+    public void OpenURL()
+    {
+        Application.OpenURL("https://forms.gle/GGUAqY9o8oL985jm8");
+        //Debug.Log("is this working?");
+    }
+    
+}
