8a6083844cf0aa7336fcdd6d2e781c6d8e8b4c83
Blythe <blytheballesteros@gmail.com>
2021-02-03 20:34:07 -1000

    sadcorgi volume worksgit add .

--- Assets/AudioController.cs
+++ Assets/AudioController.cs
@@ -9,30 +9,45 @@ public class AudioController : MonoBehaviour
 {
     private Slider musicSlide;
     private VideoPlayer video;
-    private float volume;
-    public bool doubleVol;
-    public bool halfVolume;
-    public bool threeFourthsVol;
+    // private float volume;
+    // public bool doubleVol;
+    // public bool halfVolume;
+    // public bool threeFourthsVol;
+    private GameObject menuToggler;
+    private bool changedSettings;
 
     // Start is called before the first frame update
     void Start()
     {
         musicSlide = GameObject.Find("AudioManager").GetComponent<AudioManager>().musicSlider;
+        menuToggler = GameObject.Find("/Canvi/Basic Pause Menu/toggle");
         video = GetComponent<VideoPlayer>();
-        if (doubleVol)
-        {
-            if (musicSlide.value < 0.5f) { volume = musicSlide.value * 2f; }
-            else { volume = 1f; }
-        }
-        else if (halfVolume) { volume = musicSlide.value * 0.5f; }
-        else if (threeFourthsVol) { volume = musicSlide.value * 0.75f; }
-        else { volume = musicSlide.value; }
-        video.SetDirectAudioVolume(0, volume);
+        // if (doubleVol)
+        // {
+        //     if (musicSlide.value < 0.5f) { volume = musicSlide.value * 2f; }
+        //     else { volume = 1f; }
+        // }
+        // else if (halfVolume) { volume = musicSlide.value * 0.5f; }
+        // else if (threeFourthsVol) { volume = musicSlide.value * 0.75f; }
+        // else { volume = musicSlide.value; }
+        video.SetDirectAudioVolume(0, musicSlide.value);
+        print("audio controller started");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(changedSettings && !menuToggler.activeSelf) 
+        {
+            video.SetDirectAudioVolume(0, musicSlide.value);
+            changedSettings = false;
+            print("MENU IS CLOSSEDDDD!!!");
+        }
+        else if (menuToggler.activeSelf)
+        {
+            changedSettings = true;
+            print("MENU IS OPPPEEENNN!");
+        }
+
     }
+
 }
